1d345c8caf948f68e59813ccfc6f1bb45ef4a3ec
savorn meuk <savornmeuk@gmail.com>
2021-01-30 14:42:59 -0500

    updated melpa, changed colorscheme to dracula, change font to Iosevka

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,35 +2,17 @@
 
 ;; MELPA
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;; custom theme path
-(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa")
-;; https://github.com/greduan/emacs-theme-gruvbox
-(if (display-graphic-p)
-    (load-theme 'plan9 t)
-  (load-theme 'gruvbox-dark-hard t))
+;; theme
+(load-theme 'dracula t)
 
-;; default font
-(add-to-list 'default-frame-alist
-	     '(font . "Monospace-12"))
-(set-face-attribute 'default t :font "Fira Code-12")
+;; font
+(set-face-attribute 'default nil :font "Iosevka-11")
 
 ;; disable scrollbar
 (toggle-scroll-bar -1)
@@ -45,7 +27,10 @@ There are two things you can do about this warning:
 (setq ring-bell-function 'ignore)
 
 ;; display column number
-(column-number-mode t)
+(global-linum-mode 1)
+
+;; linum format
+(setq linum-format "%d ")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -53,17 +38,8 @@ There are two things you can do about this warning:
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("30289fa8d502f71a392f40a0941a83842152a68c54ad69e0638ef52f04777a4c" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "3770d0ae70172461ee0a02edcff71b7d480dc54066e8960d8de9367d12171efb" default)))
- '(package-selected-packages
-   (quote
-    (php-mode alect-themes ess lua-mode plan9-theme gruvbox-theme)))
- '(safe-local-variable-values
-   (quote
-    ((eval when
-	   (fboundp
-	    (quote rainbow-mode))
-	   (rainbow-mode 1))))))
+   '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default))
+ '(package-selected-packages '(dracula-theme solarized-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
